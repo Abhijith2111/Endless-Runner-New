7cc146069f71081e84f61c710aef1605a8462793
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-05-18 13:02:17 +0200

    Players speed incr

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -10,7 +10,7 @@ public class PlayerMovment : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     
