1c8785da4ca56ce7eb0e85a9295a1ab9085d3107
Tristan-Robinson <ST10439581@vgconnect.edu.za>
2025-05-19 19:10:57 +0200

    Boss

--- Assets/Scripts/BossLeve/BossController.cs
+++ Assets/Scripts/BossLeve/BossController.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using System.Collections;
 
 public class BossController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject laserPrefab;
+    public GameObject minePrefab;
+    public float laserInterval = 2f;
+    public float mineInterval = 3f;
+    public Transform[] laserSpawnPoints;
+
+    private float bossTimer = 0f;
+    private float laserTimer = 0f;
+    private float mineTimer = 0f;
+
     void Start()
     {
-        
+        // Start boss behavior
+        StartCoroutine(BossBehavior());
     }
 
-    // Update is called once per frame
+    IEnumerator BossBehavior()
+    {
+        // Boss lives for 30 seconds
+        yield return new WaitForSeconds(30f);
+        Destroy(gameObject);
+    }
+
     void Update()
     {
-        
+        bossTimer += Time.deltaTime;
+        laserTimer += Time.deltaTime;
+        mineTimer += Time.deltaTime;
+
+        // Shoot lasers at intervals
+        if (laserTimer >= laserInterval)
+        {
+            ShootLasers();
+            laserTimer = 0f;
+        }
+
+        // Drop mines at intervals
+        if (mineTimer >= mineInterval)
+        {
+            DropMine();
+            mineTimer = 0f;
+        }
+    }
+
+    void ShootLasers()
+    {
+        foreach (Transform spawnPoint in laserSpawnPoints)
+        {
+            Instantiate(laserPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+
+    void DropMine()
+    {
+        Vector3 minePosition = transform.position + new Vector3(Random.Range(-5f, 5f), 0, 0);
+        Instantiate(minePrefab, minePosition, Quaternion.identity);
     }
 }
