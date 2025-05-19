be419432c298835d4897c3a4c0e6d94c3edb505d
Tristan-Robinson <ST10439581@vgconnect.edu.za>
2025-05-19 18:26:40 +0200

    les goooo

--- Assets/Scripts/CoinCollect.cs
+++ Assets/Scripts/CoinCollect.cs
@@ -4,6 +4,7 @@ public class CoinCollect : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
+        //collect coins
         Knowledge.coinTally += 1;
         this.gameObject.SetActive(false);
     }
