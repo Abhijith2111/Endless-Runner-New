2dd98766b1566de4f8c4e216ba480b504d8b2d2c
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-06-18 20:24:08 +0200

    Fixed teleporter to 1st Boss Fight

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad = "YourSceneName";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float delay = Random.Range(10f, 30f);
+        Debug.Log("Switching scene in " + delay + " seconds.");
+        Invoke(nameof(SwitchScene), delay);
+    }
+
+    private void SwitchScene()
+    {
+        SceneManager.LoadScene("Boss fight");
+    }
+
+}
