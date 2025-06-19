34e7802ed52014392a39f43988a03da2c635259c
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-06-19 21:13:46 +0200

    Player Movement in boss area1 fixed

---
+++ Assets/Scripts/BossLevel/PlayerMoveInBoss1.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerMoveInBoss1 : MonoBehaviour
+{
+    public float playerSpeed = 2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
+    }
+}
