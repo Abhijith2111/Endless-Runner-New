5e089934df7ddf9a378aaeec9ed0765b4c204772
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-06-19 22:32:35 +0200

    Player in boss fight can move left and right

--- Assets/Scripts/BossLevel/PlayerMoveInBoss1.cs
+++ Assets/Scripts/BossLevel/PlayerMoveInBoss1.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class PlayerMoveInBoss1 : MonoBehaviour
 {
     public float playerSpeed = 2;
-
+    public float horizontalSpeed = 3;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed);
+        }
     }
 }// potato
