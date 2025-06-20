580edc7f4e98e469907a63925a684ae14d9a39b6
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-06-20 14:36:34 +0200

    Limits for where the player can move in boss1 and level2 runner

--- Assets/Scripts/BossLevel/PlayerMoveInBoss1.cs
+++ Assets/Scripts/BossLevel/PlayerMoveInBoss1.cs
@@ -4,12 +4,26 @@ public class PlayerMoveInBoss1 : MonoBehaviour
 {
     public float playerSpeed = 2;
     public float horizontalSpeed = 3;
-    public float rightLimit = 5.5f;
-    public float leftLimit = -5.5f;
+    public float rightLimit = 7f;
+    public float leftLimit = -7f;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (this.gameObject.transform.position.x > leftLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (this.gameObject.transform.position.x < rightLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            }
+        }
     }
 }
