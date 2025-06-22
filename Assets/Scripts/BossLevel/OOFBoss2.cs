2dc66562b104e7251904b4611413fa5e8bf571d5
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-06-22 11:52:07 +0200

    Added colision sound and fade out anim on boss 1

--- Assets/Scripts/BossLevel/OOFBoss2.cs
+++ Assets/Scripts/BossLevel/OOFBoss2.cs
@@ -5,8 +5,10 @@ public class OOFBoss2 : MonoBehaviour
 
     [SerializeField] GameObject Player;
     [SerializeField] GameObject playerAnimation;
+    [SerializeField] AudioSource collisionFX;
     private void OnTriggerEnter(Collider other)
     {
+        collisionFX.Play();
         if(other.gameObject == Player)
         {
             Player.GetComponent<PlayerMoveInBoss1>().enabled = false;
