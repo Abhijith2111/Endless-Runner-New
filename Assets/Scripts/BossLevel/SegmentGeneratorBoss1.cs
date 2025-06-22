b5bc1b42946ceee56c6877db7fba3347c0ea2eef
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-06-22 13:45:26 +0200

    Segements now delete in boss fight

--- Assets/Scripts/BossLevel/SegmentGeneratorBoss1.cs
+++ Assets/Scripts/BossLevel/SegmentGeneratorBoss1.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SegmentGeneratorBoss1 : MonoBehaviour
@@ -9,22 +10,48 @@ public class SegmentGeneratorBoss1 : MonoBehaviour
     [SerializeField] bool creatingSegment = false;
     [SerializeField] int segmentNum;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Queue<GameObject> segmentQueue = new Queue<GameObject>();
+    private bool deletingStarted = false;
+
     void Update()
     {
-        if(creatingSegment == false)
+        if (!creatingSegment)
         {
             creatingSegment = true;
             StartCoroutine(SegmentGen());
         }
     }
+
     IEnumerator SegmentGen()
     {
-        segmentNum = Random.Range(0, 8);
-        Instantiate(segment[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        segmentNum = Random.Range(0, segment.Length);
+        GameObject newSegment = Instantiate(segment[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        segmentQueue.Enqueue(newSegment);
         zPos += 50;
-        yield return new WaitForSeconds(3);
-        creatingSegment = false;
 
+        if (!deletingStarted)
+        {
+            deletingStarted = true;
+            StartCoroutine(DeleteSegmentsOneByOne());
+        }
+
+        yield return new WaitForSeconds(5f);
+        creatingSegment = false;
+    }
+
+    IEnumerator DeleteSegmentsOneByOne()
+    {
+        yield return new WaitForSeconds(15f);
+
+        while (true)
+        {
+            if (segmentQueue.Count > 0)
+            {
+                GameObject oldest = segmentQueue.Dequeue();
+                Destroy(oldest);
+            }
+
+            yield return new WaitForSeconds(15f); 
+        }
     }
 }
